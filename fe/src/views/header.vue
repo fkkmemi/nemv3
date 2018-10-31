0e7f2086d589c5ddf1860e6ca40cc3f66241b294
fkkmemi <fkkmemi@gmail.com>
2018-10-31 16:53:34 +0900

    47 test

--- fe/src/views/header.vue
+++ fe/src/views/header.vue
@@ -7,6 +7,7 @@
     <v-btn @click="lsRemove">로컬스토리지 지우기</v-btn>
     <v-btn @click="lsClear">로컬스토리지 전체삭제</v-btn> -->
     <v-btn @click="apiWithToken">토큰과 함께 전송</v-btn>
+    <v-btn @click="apiWithTrash">이상한 문자와 함께 전송</v-btn>
   </v-container>
 </template>
 <script>
@@ -42,6 +43,11 @@ export default {
     apiWithToken () {
       const token = localStorage.getItem('token')
       axios.get(`${this.$apiRootPath}test`, { headers: { Authorization: token } })
+        .then(r => console.log(r.data))
+        .catch(e => console.log(e.message))
+    },
+    apiWithTrash () {
+      axios.get(`${this.$apiRootPath}test`, { headers: { Authorization: 'abcdefghijk' } })
         .then(r => console.log(r.data))
         .catch(e => console.log(e.message))
     }

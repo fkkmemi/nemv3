3169118a5d42039c3050b440bac721f143fbef2c
fkkmemi <fkkmemi@gmail.com>
2018-10-31 09:45:29 +0900

    promise async test

--- be/app.js
+++ be/app.js
@@ -42,7 +42,6 @@ console.log(`${process.env.NODE_ENV} started!`)
 
 
 const cfg = require('../config')
-console.log(cfg)
 
 mongoose.connect(cfg.dbUrl, { useNewUrlParser: true }, (err) => {
   if (err) return console.error(err)
@@ -74,3 +73,145 @@ mongoose.connect(cfg.dbUrl, { useNewUrlParser: true }, (err) => {
 
 
 module.exports = app;
+
+var jwt = require('jsonwebtoken');
+const key = '베리베리어려운키'
+// var token = jwt.sign({ id: 'memi', email: 'memi@xxx.com' }, key);
+// console.log(token)
+//
+// jwt.verify(token, 'ㄴㄹㅈㄷㄹ', (err, r) => {
+//   if (err) return console.error(err.message)
+//   console.log(r) // bar
+//   console.log(new Date(r.iat * 1000).toLocaleString())
+// })
+
+
+// console.log(jwt.sign({ id: 'memi' }, 'abcd'))
+// console.log('헬로?')
+// jwt.sign({ id: 'memi'}, 'key', (err, token) => {
+//   console.log(token)
+// })
+// console.log('헬로?')
+
+// User.findOne({}, (err, r) => {
+//   if (err) return console.error(err)
+//   console.log(r)
+// })
+// User.findOne({})
+//   .then(r => console.log(r))
+//   .catch(err => console.error(err))
+
+
+// 콜백
+// User.findOne({ name: 'aaa'}, (err, u) => {
+//   if (err) return console.error(err.message)
+//   if (!u) {
+//     console.log(u) // null
+//     User.create({ name: 'aaa', age: 10 }, (err, cu) => {
+//       if (err) return console.error(err.message)
+//       console.log(cu) // { name: 'aaa', age: 10, _id: 5bd81974ad66cf3832db0838, __v: 0 }
+//       jwt.sign({ name: cu.name, age: cu.age}, key, (err, token) => {
+//         if (err) return console.error(err.message)
+//         console.log(token) // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+//         jwt.verify(token, key, (err, v) => {
+//           if (err) return console.error(err.message)
+//           console.log(v) // { name: 'aaa', age: 10, iat: 1540888948 }
+//         })
+//       })
+//     })
+//   }
+//   else {
+//     console.log(u) // { name: 'aaa', age: 10, _id: 5bd81974ad66cf3832db0838, __v: 0 }
+//     const user = u
+//     User.updateOne({ _id: u._id }, { $inc: { age: 1 }}, (err, ur) => {
+//       if (err) return console.error(err.message)
+//       console.log(ur) // { n: 1, nModified: 1, ... }
+//       jwt.sign({ name: user.name, age: user.age + 1 }, key, (err, token) => {
+//         if (err) return console.error(err.message)
+//         console.log(token) // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+//         jwt.verify(token, key, (err, v) => {
+//           if (err) return console.error(err.message)
+//           console.log(v) // { name: 'aaa', age: 11, iat: 1540889007 }
+//         })
+//       })
+//     })
+//   }
+// })
+
+// User.deleteMany({})
+//   .then(r => console.log(r))
+//   .catch(err => console.error(err))
+
+const signToken = (u, k) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign({ name: u.name, age: u.age }, k, (err, token) => {
+      if (err) reject(err)
+      resolve(token)
+    })
+  })
+}
+const verifyToken = (t, k) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(t, k, (err, v) => {
+      if (err) reject(err)
+      resolve(v)
+    })
+  })
+}
+
+// 프라미스
+// let user
+// User.findOne({ name: 'aaa' })
+//   .then((u) => {
+//     if (!u) return User.create({ name: 'aaa', age: 10 })
+//     return Promise.resolve(u)
+//   })
+//   .then((u) => {
+//     user = u
+//     return User.updateOne({ _id: u._id }, { $inc: { age: 1 }})
+//   })
+//   .then((r) => {
+//     if (!r.nModified) throw new Error('수정된 것이 없네요..')
+//     user.age++
+//     return signToken(user, key)
+//   })
+//   .then((token) => {
+//     return verifyToken(token, key)
+//   })
+//   .then(v => console.log(v))
+//   .catch((err) => {
+//     console.error(err.message)
+//   })
+
+// 어씽크 테스트
+
+// const asyncTest = async (i) => {
+//   if (i > 10) throw new Error('10보다 큰건 싫어요')
+//   return i + 2
+// }
+//
+// asyncTest(1)
+//   .then(r => console.log(`2 더해져서 ${r} 입니다.`))
+//   .catch(e => console.error(`에러났네요: ${e.message}`))
+//
+// asyncTest(11)
+//   .then(r => console.log(`2 더해져서 ${r} 입니다.`))
+//   .catch(e => console.error(`에러났네요: ${e.message}`))
+
+// 어씽크
+
+const getToken = async (name) => {
+  let u = await User.findOne({ name }) // name: name을 축약하고 await로 기다려 줍니다.
+  if (!u) u = await User.create({ name , age: 10 }) // 만들어주고 u를 갱신 합니다.
+  if (u.age > 12) throw new Error(`${u.age}는 나이가 너무 많습니다.`)
+  const ur = await User.updateOne({ _id: u._id }, { $inc: { age: 1 }}) // age를 증가시키고 ur(user result)에 결과값을 담아놓습니다.
+  if (!ur.nModified) throw new Error('수정된 것이 없네요..') // 수정된 값이 없다면 에러와 함께 내보냅니다.
+  u = await User.findOne({ _id: u._id }) // age가 증가 된 것으로 갱신해줍니다.
+  const token = await signToken(u, key) // 받아 두었던 u로 토큰을 만듭니다.
+  const v = await verifyToken(token, key)
+  return v
+}
+
+getToken('aaa')
+  .then(v => console.log(v))
+  .catch(err => console.error(err.message))
--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -2,6 +2,30 @@ var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
 
+router.all('*', function(req, res, next) {
+  console.log(req.headers)
+  console.log(req.path)
+
+  if (req.path === '/xxx') return res.send({ status: 'OK' })
+
+  next()
+});
+
+router.all('*', (req, res, next) => {
+  req.x = '보안 인증 통과'
+  next()
+})
+
+
+router.all('*', (req, res, next) => {
+  req.x = req.x + ' 2번째 인증 또한 통과'
+  next()
+})
+
+router.all('*', (req, res, next) => {
+  res.send({ status: req.x })
+})
+
 router.use('/test', require('./test'))
 router.use('/user', require('./user'))
 

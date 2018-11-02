982c41183462f8931e992b07336c9c9c85e73246
fkkmemi <fkkmemi@gmail.com>
2018-11-02 21:19:18 +0900

    51 bug fix

--- be/routes/api/page/index.js
+++ be/routes/api/page/index.js
@@ -4,10 +4,7 @@ var router = express.Router();
 const Page = require('../../../models/pages')
 
 router.post('/', function(req, res, next) {
-  // if (!req.user) return res.send({ success: true, d: { id: '손님', name: '손님', lv: 3 } })
   const { name } = req.body
-  // console.log('here')
-  // console.log(req.user)
   Page.findOne({ name })
     .then((r) => {
       if (!r) {
@@ -15,7 +12,7 @@ router.post('/', function(req, res, next) {
         return Page.create({ name })
       }
       if (r.lv < req.user.lv) throw new Error(`페이지 ${name} 이용 자격이 없습니다.`)
-      return Page.updateOne({ _id: r._id }, { $inc: { viewCnt: 1 } })
+      return Page.updateOne({ _id: r._id }, { $inc: { inCnt: 1 } })
     })
     .then(() => {
     //   return Page.find()
@@ -28,21 +25,6 @@ router.post('/', function(req, res, next) {
       res.send({ success: false, msg: e.message })
     })
 });
-// Page.updateOne({ name: '/' }, { $set: { lv: 3 }})
-//   .then((rs) => {
-//     console.log(rs)
-//   })
-//   .catch((e) => {
-//     console.error(e.message)
-//   })
-
-// Page.find()
-//   .then((rs) => {
-//     console.log(rs)
-//   })
-//   .catch((e) => {
-//     console.error(e.message)
-//   })
 
 router.all('*', function(req, res, next) {
   next(createError(404, '그런 api 없어'));

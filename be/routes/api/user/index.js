b0d3d3b0fb5da5715fe2221140c3ac7f9c1bc14c
fkkmemi <fkkmemi@gmail.com>
2018-12-12 17:02:20 +0900

    84 image convert 2

--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -2,16 +2,23 @@ const router = require('express').Router()
 const createError = require('http-errors')
 const multer = require('multer')
 const imgConvert = require('base64-img')
+const sharp = require('sharp')
 const fs = require('fs')
 
 router.post('/', multer({ dest: 'public/' }).single('bin') ,(req, res, next) => {
   console.log(req.body)
   console.log(req.file)
 
-  imgConvert.base64(req.file.path, (err, fd) => {
+  const fn = req.file.path + 'sharp'
+  sharp(req.file.path).resize(200,200).crop(sharp.strategy.entropy).toFile(fn, (err, info) => {
     if (err) return next(err)
-    fs.unlinkSync(req.file.path)
-    res.send(fd.toString())
+    imgConvert.base64(fn, (err, fd) => {
+      if (err) return next(err)
+      fs.unlinkSync(req.file.path)
+      fs.unlinkSync(fn)
+      console.log(fd.length)
+      res.send(fd.toString())
+    })
   })
 })
 
